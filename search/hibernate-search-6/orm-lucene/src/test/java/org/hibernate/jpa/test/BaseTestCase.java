9678731adea823df145415b6c6e6f5216c6fac93
Matthew Frederes <matt@bitech.biz>
2021-10-11 22:29:47 -0700

    add a base class to enable testing of lazy loading

---
+++ search/hibernate-search-6/orm-lucene/src/test/java/org/hibernate/jpa/test/BaseTestCase.java
@@ -0,0 +1,130 @@
+package org.hibernate.jpa.test;
+
+import org.hibernate.Session;
+import org.hibernate.boot.registry.internal.StandardServiceRegistryImpl;
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.cfg.Environment;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.search.mapper.orm.Search;
+import org.hibernate.search.mapper.orm.session.SearchSession;
+import org.hibernate.search.mapper.orm.work.SearchIndexingPlan;
+import org.hibernate.testing.junit4.CustomRunner;
+import org.jboss.logging.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+@RunWith( CustomRunner.class )
+public abstract class BaseTestCase  {
+
+	protected final Logger log = Logger.getLogger( getClass() );
+
+	private StandardServiceRegistryImpl serviceRegistry;
+	private SessionFactoryImplementor entityManagerFactory;
+
+	protected EntityManager em;
+	private ArrayList<EntityManager> isolatedEms = new ArrayList<EntityManager>();
+	EntityTransaction userTransaction;
+
+	@Before
+	@SuppressWarnings( {"UnusedDeclaration"})
+	public void buildEntityManagerFactory() {
+		log.trace( "Building EntityManagerFactory" );
+
+		entityManagerFactory = Persistence.createEntityManagerFactory("templatePU", getConfig()).unwrap(SessionFactoryImplementor.class);
+		EntityManager em = entityManagerFactory.createEntityManager();
+
+		serviceRegistry = (StandardServiceRegistryImpl) entityManagerFactory.getServiceRegistry().getParentServiceRegistry();
+
+	}
+
+	protected Map getConfig() {
+		Map<Object, Object> config = Environment.getProperties();
+
+		Collection<ClassLoader> list = new ArrayList<>();
+		list.add(getClass().getClassLoader());
+		config.put( AvailableSettings.CLASSLOADERS, list );
+		return config;
+	}
+
+	@After
+	@SuppressWarnings( {"UnusedDeclaration"})
+	public void releaseResources() {
+		try {
+			releaseUnclosedEntityManagers();
+		}
+		finally {
+			if ( entityManagerFactory != null && entityManagerFactory.isOpen()) {
+				entityManagerFactory.close();
+			}
+		}
+	}
+
+	private void releaseUnclosedEntityManagers() {
+		releaseUnclosedEntityManager( this.em );
+
+		for ( EntityManager isolatedEm : isolatedEms ) {
+			releaseUnclosedEntityManager( isolatedEm );
+		}
+	}
+
+	private void releaseUnclosedEntityManager(EntityManager em) {
+		if ( em == null ) {
+			return;
+		}
+		if ( !em.isOpen() ) {
+			return;
+		}
+
+		if ( em.getTransaction().isActive() ) {
+			em.getTransaction().rollback();
+            log.warn("You left an open transaction! Fix your test case. For now, we are closing it for you.");
+		}
+		if ( em.isOpen() ) {
+			em.close();
+            log.warn("The EntityManager is not closed. Closing it.");
+		}
+	}
+
+	protected Session getDelegate() {
+		Session s = (Session) em.getDelegate();
+		return s;
+	}
+
+	protected EntityManager getOrCreateEntityManager() {
+		if ( em == null || !em.isOpen() ) {
+			em = entityManagerFactory.createEntityManager();
+		}
+		return em;
+	}
+
+	protected void startTransaction() {
+		em = getOrCreateEntityManager();
+		userTransaction = em.getTransaction();
+		userTransaction.begin();
+	}
+
+	protected void endTransaction() {
+		userTransaction.commit();
+		em.close();
+	}
+
+	protected void index(Object object) {
+		SearchSession searchSession = Search.session( em );
+		SearchIndexingPlan indexingPlan = searchSession.indexingPlan();
+
+		try {
+			indexingPlan.addOrUpdate( object );
+		}
+		catch (RuntimeException e) {
+			System.out.println("[GenericFlexBean] Indexing issue: " + e.getMessage());
+		}
+	}
+}
